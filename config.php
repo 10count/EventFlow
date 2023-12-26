0e53b9eec9bc23dec76dde8aa1465bd9b3bc396d
Kim Anna <154206849+10count@users.noreply.github.com>
2023-12-26 13:43:50 +0900

    Update config.php

--- config.php
+++ config.php
@@ -1,17 +1,10 @@
 <?php
-    //XAMMP
-    
-    $host = "localhost";
-    $dbid = "root";
-    $dbpass = "";
-    $dbname = "eventflow_db";
-    
-
-    //infinity free
+    //removed on purpose
+    //infinityfree
     /*
-    $host = "sql105.infinityfree.com";
-    $dbid = "if0_35673620";
-    $dbpass = "pJ7BL85w6jMudKI";
-    $dbname = "if0_35673620_eventflow_db";
+    $host = "";
+    $dbid = "";
+    $dbpass = "";
+    $dbname = "";
     */
 ?>
